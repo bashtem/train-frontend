cfd568dc5adf89d50369543b685c6edca2ba3fd2
Taiwo Temitope Basit <ttaiwo@quidax.com>
2024-06-05 23:45:47 +0100

    updates on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import { Box, Flex, Button, Text, HStack, Image, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Button,
+  Text,
+  HStack,
+  Image,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
 import { PiCaretDownBold } from "react-icons/pi";
 import "@fontsource/rubik";
+import partners from "../app/json_data/partners.json";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -12,7 +23,7 @@ export default function Home() {
           justifyContent={"space-between"}
           align={"center"}
           h={"100%"}
-          px="9em"
+          px="8.5em"
         >
           <HStack spacing={"2em"}>
             <Button
@@ -51,7 +62,7 @@ export default function Home() {
         </Flex>
       </Box>
 
-      <Box pt={"2em"} px={"9em"}>
+      <Box pt={"2em"} px={"8.5em"}>
         <HStack
           justifyContent={"space-between"}
           align={"center"}
@@ -130,7 +141,7 @@ export default function Home() {
         </Flex>
       </Box>
 
-      <Box pt={"2em"} px={"9em"}>
+      <Box pt={"2em"} px={"8.5em"} pb={"12em"}>
         <Box>
           <Text
             width={"35%"}
@@ -142,8 +153,157 @@ export default function Home() {
           >
             Assembly partners with suppliers across Uzbekistan
           </Text>
-          <HStack pt={"3em"} spacing={3}>
-            <Image src="./assets/persil_2.svg" />
+          <HStack pt={"3em"} spacing={7}>
+            {partners.map(({ img }, index) => (
+              <Image key={index} src={img} />
+            ))}
+          </HStack>
+        </Box>
+      </Box>
+
+      <Box bg={"white"} pt={"5em"} pb={"6em"}>
+        <Box px={"8.5em"}>
+          <Flex maxHeight={"46em"}>
+            <Box bgColor={"#722F48"} width={"60%"} px={"2.5em"} color={"white"}>
+              <Text
+                pt={"4em"}
+                fontWeight={"bold"}
+                fontSize={"48px"}
+                lineHeight={1.2}
+                width={"90%"}
+                fontFamily={"Rubik"}
+              >
+                Simplified record keeping
+              </Text>
+              <Text pt={"1em"} fontSize={"18px"}>
+                Keep all your financial records in one place, making audits and
+                financial analysis a breeze.
+              </Text>
+              <HStack spacing={10} pt={"2.5em"}>
+                <Button size={"lg"}>Discover products</Button>
+                <Link
+                  textDecoration={"none"}
+                  fontWeight={"semibold"}
+                  fontSize={"16px"}
+                >
+                  Learn more
+                </Link>
+              </HStack>
+            </Box>
+            <Image
+              borderTopRightRadius={"22em"}
+              width={"50%"}
+              src="./assets/sitting.svg"
+            />
+          </Flex>
+
+          <Flex pt="4em">
+            <Image width={"50%"} src="./assets/deliver.svg" />
+            <Box bgColor={"#FFF3DB"} width={"60%"} px={"2.5em"}>
+              <Text
+                pt={"4em"}
+                fontWeight={"bold"}
+                fontSize={"48px"}
+                lineHeight={1.2}
+                width={"90%"}
+                fontFamily={"Rubik"}
+              >
+                Cost Savings
+              </Text>
+              <Text pt={"1em"} fontSize={"18px"} width={"80%"}>
+                Deal directly with suppliers and enjoy cost savings that impact
+                your profitability...
+              </Text>
+              <HStack spacing={10} pt={"2.5em"}>
+                <Button size={"lg"}>Save more, spend less</Button>
+                <Link
+                  textDecoration={"none"}
+                  fontWeight={"semibold"}
+                  fontSize={"16px"}
+                >
+                  Learn more
+                </Link>
+              </HStack>
+            </Box>
+          </Flex>
+
+          <Flex pt={"4em"}>
+            <Box bgColor={"#4D6425"} width={"60%"} px={"2.5em"} color={"white"}>
+              <Text
+                pt={"4em"}
+                fontWeight={"bold"}
+                fontSize={"48px"}
+                lineHeight={1.2}
+                width={"90%"}
+                fontFamily={"Rubik"}
+              >
+                Grow your business
+              </Text>
+              <Text pt={"1em"} fontSize={"18px"}>
+                Leverage the benefits of Train to optimize your retail business.
+                and watch your business thrive. We're here to support your
+                growth
+              </Text>
+              <HStack spacing={10} pt={"2.5em"}>
+                <Button size={"lg"}>Unlock growth</Button>
+                <Link
+                  textDecoration={"none"}
+                  fontWeight={"semibold"}
+                  fontSize={"16px"}
+                >
+                  Learn more
+                </Link>
+              </HStack>
+            </Box>
+            <Image width={"50%"} src="./assets/courier.svg" />
+          </Flex>
+          <VStack justifyContent={"center"}>
+            <Text
+              color={"#6B7280"}
+              fontSize={"14px"}
+              pt={"0.7em"}
+              fontWeight={"semibold"}
+              letterSpacing={"0.1em"}
+            >
+              TRAIN MAKES IT EASY
+            </Text>
+
+            <Text
+              fontWeight={"bold"}
+              fontSize={"36px"}
+              fontFamily={"Rubik"}
+              noOfLines={2}
+              lineHeight={1.2}
+              width={"45%"}
+              textAlign={"center"}
+            >
+              Your journey to retail success with Train
+            </Text>
+          </VStack>
+
+          <HStack>
+            <Box color={"#374151"} width={"30%"}>
+              <Text
+                fontWeight={"semibold"}
+                fontSize={"20px"}
+                fontFamily={"Rubik"}
+              >
+                Register your business
+              </Text>
+              <Text fontSize={"18px"} noOfLines={3} pt={"0.6em"}>
+                Begin your journey by registering on Train. It's a quick and
+                easy process. Just provide your basic business information to
+                get started.
+              </Text>
+              <Box>
+                <Link>
+                  Browse Template{" "}
+                  <Box as="span">
+                    <FaArrowRightLong />
+                  </Box>
+                </Link>
+              </Box>
+            </Box>
           </HStack>
         </Box>
       </Box>
